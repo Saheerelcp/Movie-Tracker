e663d3569a3c0ac6e3b215d34687939f1e6256e0
Saheerelcp <saheercp26@gmail.com>
2025-07-16 01:39:37 +0530

    'hi'

--- movieproject/movieproject/wsgi.py
+++ movieproject/movieproject/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+import django
 from django.core.wsgi import get_wsgi_application
 
+from django.core.management import call_command
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movieproject.settings')
 
+django.setup()
+call_command("migrate", interactive=False)
 application = get_wsgi_application()
